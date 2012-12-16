8ee215affd3d8a2fcc7ced9cfc71867144f71637
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2012-12-15 23:49:36 -0500

    Initial signature stuffs.

---
+++ heap/binary.hs
@@ -0,0 +1,23 @@
+-- the heap datatype
+data Heap = undefined
+
+-- insert into the heap
+insert = undefined
+
+-- delete the specified element from the heap
+delete = undefined
+
+-- turn a list into a heap
+fromList = undefined
+
+-- create a new empty heap
+empty = undefined
+
+-- create a heap of one element
+singleton = undefined
+
+-- return the smallest element of the heap
+minimum = undefined
+
+-- return the largest element of the heap
+maximum = undefined
